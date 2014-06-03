bd5161d503387e1cf4f680c73c06fc6ac2686ad9
Ciaran Downey <me@ciarand.me>
2014-06-02 21:16:14 -0700

    Fix phpbrew in Mac OS X (Darwin)

--- roles/php/files/99_phpbrew.bash
+++ roles/php/files/99_phpbrew.bash
@@ -1,1 +1,5 @@
+# load the bashrc file if it exists
 [[ -f ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
+
+# make sure we have the bin in path for composer n stuff
+export PATH=~/.phpbrew/.phpbrew/bin:$PATH
