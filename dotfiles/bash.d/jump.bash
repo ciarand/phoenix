5ea22bba67f729a07713def6d2e094050f5cdcdf
Ciaran Downey <me@ciarand.me>
2014-06-04 20:15:44 -0700

    Fix jump completion for both BSD and GNU "find"

--- dotfiles/bash.d/jump.bash
+++ dotfiles/bash.d/jump.bash
@@ -15,16 +15,17 @@ function unmark {
 }
 
 function marks {
-    ls -l "$MARKPATH" | tail -n +2 | sed 's/  / /g' | cut -d' ' -f9- | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
+    ls -l "$MARKPATH" \
+        | tail -n +2 \
+        | sed 's/  / /g' \
+        | cut -d' ' -f9- \
+        | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
 }
 
-#function marks {
-    #ls -l "$MARKPATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
-#}
-
 _completemarks() {
-  local curw=${COMP_WORDS[COMP_CWORD]}
-  local wordlist=$(find $MARKPATH -type l -printf "%f\n")
+  local curw="${COMP_WORDS[COMP_CWORD]}"
+  local wordlist="$(find $MARKPATH -type l -exec basename {} ';')"
+
   COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
   return 0
 }
