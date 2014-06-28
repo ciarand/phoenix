bf310bd26c4c55a4d17824b419ac16f7e6bc16b8
Ciaran Downey <me@ciarand.me>
2014-06-28 11:39:02 -0700

    Fix the xmodmap bash script on OS X
    Really Xmodmap shouldn't be started in OS X because ironically I don't
    use any X apps in OS X.

--- dotfiles/bash.d/99_xmodmap.bash
+++ dotfiles/bash.d/99_xmodmap.bash
@@ -1,4 +1,4 @@
 # if xmodmap exists and the file exists, "source" it
-if hash xmodmap 2>/dev/null && [ -s ~/.Xmodmap ]; then
+if [[ $(uname) -ne "Darwin" ]] && hash xmodmap 2>/dev/null && [ -s ~/.Xmodmap ]; then
     xmodmap ~/.Xmodmap
 fi
