b3da02ba866f51abb4c805efe999cdfb9191f0eb
Ciaran Downey <me@ciarand.me>
2014-07-23 18:33:33 -0700

    Remove dotfiles

--- dotfiles/bash.d/00_path.bash
+++
@@ -1,7 +0,0 @@
-# reset the path
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-# source etc/profile if it exists
-[ -f /etc/profile ] && source /etc/profile
-
-export PATH=~/bin:~/managed-bin:/usr/local/bin:/usr/local/sbin:$PATH
--- dotfiles/bash.d/99_xmodmap.bash
+++
@@ -1,4 +0,0 @@
-# if xmodmap exists and the file exists, "source" it
-if [[ $(uname) -ne "Darwin" ]] && hash xmodmap 2>/dev/null && [ -s ~/.Xmodmap ]; then
-    xmodmap ~/.Xmodmap
-fi
